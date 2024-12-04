f279528a041bde3387c166e811d06972697388ca
vtiads <0903djds@naver.com>
2024-12-04 14:56:49 +0900

    README 파일 업데이트, 계산기main.c 파일첫번째 숫자 입력 추가

--- main.c
+++ main.c
@@ -8,16 +8,29 @@ void subtract(double a, double b) {
     printf("뺄셈 결과: %.2f\n", a - b);
 }
 
+void multiply(double a, double b) {
+    printf("곱셈 결과: %.2f\n", a * b);
+}
+
+void divide(double a, double b) {
+    if (b == 0) {
+        printf("나누기 오류: 0으로 나눌 수 없습니다.\n");
+    }
+    else {
+        printf("나눗셈 결과: %.2f\n", a / b);
+    }
+}
+
 int main() {
     double num1, num2;
-    printf("첫 번째 숫자를 입력하세요: ");
-    scanf("%lf", &num1);
-    
-    printf("두 번째 숫자를 입력하세요: ");
-    scanf("%lf", &num2);
+    int choice;
 
-    add(num1, num2);
-    subtract(num1, num2);
-    return 0;
-}
+    // 첫 번째 숫자 입력
+    printf("첫 번째 숫자를 입력하세요: ");
+    if (scanf("%lf", &num1) != 1) {
+        printf("잘못된 입력입니다.\n");
+        return 1; // 오류가 발생하면 프로그램 종료
+    }
+
+
 
